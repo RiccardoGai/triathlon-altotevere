54a8078801eee504ae858d6b7037c7fa158082dd
Riccardo Gai <r.gai@sintraconsulting.eu>
2023-11-04 18:06:33 +0100

    updated tina config

--- tina/config.ts
+++ tina/config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'tinacms';
 import Global from './collections/global';
 import Page from './collections/page';
 
-// Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.BRANCH || process.env.VERCEL_GIT_COMMIT_REF || 'main';
-
 export default defineConfig({
-  branch,
-  clientId: process.env.TINA_CLIENT_ID,
-  token: process.env.TINA_TOKEN, // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
+  branch:
+    process.env.NEXT_PUBLIC_TINA_BRANCH! ||
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! ||
+    process.env.HEAD!,
+  token: process.env.TINA_TOKEN!,
 
   build: {
     outputFolder: 'admin',
