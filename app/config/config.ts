2527c6cdf201ee1920d35923c6502434f5f80e62
Riccardo Gai <r.gai@sintraconsulting.eu>
2024-09-25 15:05:10 +0200

    fix

--- app/config/config.ts
+++ app/config/config.ts
@@ -1,5 +1,3 @@
-import { IubendaCookieSolutionBannerConfigInterface } from '@mep-agency/next-iubenda';
-
 export const CONFIG = {
   SITE_URL:
     process.env.NEXT_PUBLIC_SITE_URL ??
@@ -9,5 +7,5 @@ export const CONFIG = {
     siteId: 3768958,
     cookiePolicyId: 12131529,
     lang: 'it'
-  } as IubendaCookieSolutionBannerConfigInterface
+  }
 };
