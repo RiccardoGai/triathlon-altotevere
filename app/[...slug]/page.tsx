4567a2e8eec419bb6921b147530460409d522c46
Riccardo Gai <riccardo.gai91@gmail.com>
2024-10-10 18:07:37 +0200

    fix not found

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,5 +1,6 @@
 import client from '@tina-client';
 import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 import path from 'path';
 import PageBlock from '../components/blocks/page-block.component';
 import { CONFIG } from '../config/config';
@@ -17,31 +18,41 @@ export async function generateMetadata({
 }: {
   params: { slug: string[] };
 }): Promise<Metadata> {
-  const page = await client.queries.page({
-    relativePath: `${path.join(...params.slug)}.mdx`
-  });
-  const seo = page.data.page.seo;
-  return {
-    title: seo?.title,
-    description: seo?.description,
-    keywords: seo?.keywords as string[],
-    openGraph: {
-      type: 'website',
-      title: seo?.title as string,
-      description: seo?.description as string,
-      url: path.join(CONFIG.SITE_URL, ...params.slug)
-    },
-    twitter: {
-      title: seo?.title as string,
-      description: seo?.description as string,
-      card: 'summary_large_image'
-    }
-  };
+  try {
+    const page = await client.queries.page({
+      relativePath: `${path.join(...params.slug)}.mdx`
+    });
+    const seo = page.data.page.seo;
+    return {
+      title: seo?.title,
+      description: seo?.description,
+      keywords: seo?.keywords as string[],
+      openGraph: {
+        type: 'website',
+        title: seo?.title as string,
+        description: seo?.description as string,
+        url: path.join(CONFIG.SITE_URL, ...params.slug)
+      },
+      twitter: {
+        title: seo?.title as string,
+        description: seo?.description as string,
+        card: 'summary_large_image'
+      }
+    };
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
 }
 
 export default async function Page({ params }: { params: { slug: string[] } }) {
-  const page = await client.queries.page({
-    relativePath: `${path.join(...params.slug)}.mdx`
-  });
-  return <PageBlock props={page} />;
+  try {
+    const page = await client.queries.page({
+      relativePath: `${path.join(...params.slug)}.mdx`
+    });
+    return <PageBlock props={page} />;
+  } catch (error) {
+    console.error(error);
+    notFound();
+  }
 }
