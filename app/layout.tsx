df0b10bf4d2ec4d73631da188c37f46feef92fbc
Riccardo Gai <r.gai@sintraconsulting.eu>
2025-03-04 11:55:56 +0100

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="it">
       <head>
         <meta name="google-site-verification" content="yQHdS31nK4yZ5sUdZam4iHF3VQ-hscbZ3fZzZe4AcDY" />
+        <meta name="google-adsense-account" content="ca-pub-7831086226943392" />
       </head>
       <body className={`${font.className} antialiased text-default bg-page tracking-tight flex flex-col`}>
         <IubendaProvider bannerConfig={CONFIG.IUBENDA}>
